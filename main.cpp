e894f91c624cc07d758a2f3866f24b8b261737ef
cwy <2103370@1utar.my>
2023-04-15 13:57:52 +0800

    Fixed some bugs and modified the struct

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 const int MAX_STUDENTS = 100; // Maximum number of students that can be stored
 const int MAX_COURSES = 50; // Maximum number of courses that can be stored
+const int MAX_LECTURERS = 50; // Maximum number of lecturers that can be stored
 
 /// @brief Course that provided
 struct Course {
@@ -15,8 +16,8 @@ struct Course {
     int credit_hour; // course credit hour (etc: 4)
 };
 
-/// @brief Struct representing student
-struct Student {
+/// @brief Struct representing identity, including students and lecturers
+struct People {
     string id; // Student id
     string name; // Student name
     string password; // Student password in hash
@@ -25,35 +26,120 @@ struct Student {
     int total_credit_hour; // Total credit hour
 };
 
-struct Lecturer {
-    string id; // Lecturer id
-    string name; // Lecturer name
-    string password; // Lecturer password in hash
-};
-
 // Function declaration
 
-int split(string, char, string []);
-void readFile(string, Student[] , int&);
-void writeFile(string, Student[], int);
-void displayStudentInfo(Student[], int);
+void readFile(string, People[], int&, bool);
+void writeFile(string, People[], int, bool);
+void displayStudentInfo(People[], int);
 void mainMenu(bool&);
 void studentMenu();
 
 
 // Main function
 int main() {
-    Student students[MAX_STUDENTS]; // Array to store student data
-    int student_num;
-    readFile("students_list.txt", students, student_num); // Current number of students in the array
+    People students[MAX_STUDENTS]; // Array to store People data
+    People lecturers[MAX_LECTURERS]; // Array to store lecturer data
+    // Read file and get current number of students in the array
+    int student_num, lecturer_num;
+    readFile("students_list.txt", students, student_num, true);
+    readFile("lecturer_list.txt", lecturers, lecturer_num, false);
+    for (int i = 0; i < student_num; i++) {
+        cout << students[i].name << endl;
+        cout << students[i].total_credit_hour << endl;
+    }
     bool exit = false;
     // while (!exit) {
     //     mainMenu(exit);
     //     system("pause");
     // }
-    writeFile("students_list_output.txt", students, student_num);
-
+    writeFile("students_list_output.txt", students, student_num, true);
+    writeFile("lecturer_list_output.txt", lecturers, lecturer_num, false);
     return 0;
+}
+
+/**
+ * @brief Read people info from targeted file
+ * @param file_name Input file to be read
+ * @param people The array to store data
+ * @param count The number of people in the array
+ * @param isStudent Differentiate between student and other identity
+*/
+void readFile(string file_name, People people[], int& count, bool isStudent = false) {
+    
+    // if the file does not exist create it, else work with it
+    fstream file(file_name, ios::app | ios::in);
+
+    // start reading
+    // get the id, name and password
+    for (count = 0; !file.eof(); count++) {
+
+        getline(file, people[count].id, '|');
+        getline(file, people[count].name, '|');
+
+        if (isStudent) {
+            // get the password
+            getline(file, people[count].password, '|');
+
+            // read course info
+            file >> people[count].course_num;
+            file.ignore();
+            people[count].total_credit_hour = 0;
+            // loop every course of student and read the info
+            for (int j = 0; j < people[count].course_num ; j++) {
+                getline(file, people[count].course[j].code, '|');
+                getline(file, people[count].course[j].name, '|');
+                getline(file, people[count].course[j].gred);
+                cout << people[count].course[j].code << endl;
+                people[count].course[j].credit_hour = stoi(people[count].course[j].code.substr(7, 1));
+                cout << people[count].course[j].credit_hour << endl;
+                people[count].total_credit_hour += people[count].course[j].credit_hour;
+            }
+        }
+        else {
+            getline(file, people[count].password);
+        }
+    }
+    // done reading people info
+    file.close();
+}
+
+/**
+ * @brief Write students info into targeted file
+ * @param file_name Output file to be write
+ * @param students Student array to write
+ * @param student_num Number of students
+*/
+void writeFile(string file_name, People people[], int count, bool isStudent = false) {
+
+    fstream file(file_name, ios::out);
+
+    for (int i = 0; i < count; i++) {
+        // write people info
+        file << people[i].id << '|';
+        file << people[i].name << '|';
+        
+        // if student, need to write course info
+        if (isStudent) {
+            file << people[i].password << '|';
+            file << people[i].course_num;
+            //write course info
+            for (int j = 0; j < people[i].course_num; j++) {
+                file << endl;
+                file << people[i].course[j].code << '|';
+                file << people[i].course[j].name << '|';
+                file << people[i].course[j].gred;
+            }
+        }
+        else {
+            file << people[i].password;
+        }
+
+        // if not last people, write new line
+        if (i != count - 1){
+            file << endl;
+        }
+    }
+    file.close();
 }
 
 void mainMenu(bool& exit) {
@@ -87,7 +173,6 @@ void mainMenu(bool& exit) {
         cout << "Invalid option" << endl;
     }
 }
-
 
 void courseMenu(Course courses[], int& course_num, bool is_lecturer = false) {
 
@@ -137,111 +222,22 @@ void studentMenu() {
 }
 
 /**
- * @brief Split string into substring by delimiter
- * @param str Input string to be split
- * @param delim Delimiter between substring
- * @param substr_arr Output string array that contains the substrings
- * @return Numer of substring
+ * @brief Verify the login
+ * @param password Correct password
+ * @return True if the password is correct, false otherwise
 */
-int split(string str, char delim, string substr_arr[]) {
+bool login(string password) {
+    
+    string input;
+    cout << "Please enter your password : ";
+    getline(cin, input);
 
-    int n = 0; // number of substring
-    int length = str.size(); // length of string
-    if (length == 0) {
-        return 0; // if the string is empty, return
+    if (input == password) {
+        return true;
     }
     else {
-        int start = 0, end; // start and end position to get the substr
-        do {
-            end = str.find(delim, start);
-            if (end != -1) {
-                substr_arr[n] = str.substr(start, end - start);
-            }
-            else {
-                substr_arr[n] = str.substr(start, length - start +1);
-            }
-            start = end + 1;
-            n++;
-        } while(end != -1);
+        cout << "Wrong password" << endl;
+        return false;
     }
 
-    return n;
-}
-
-bool login() {
-    return false;
-}
-
-/**
- * @brief Read stuents info from students file
- * @param file_name Input file name
- * @param students Output array to store studdents' info
- * @param n Total number of students
- * @param i Total number of courses
- * 
-*/
-void readFile(string file_name, Student students[], int& n) {
-    fstream file;
-    n = 0; // number of student
-    string courses;
-    
-    fstream(file_name, ios::app); // if the file does not exist, create it
-    file.open(file_name);
-    // read student info
-    // while there are still student, get the id, name and password
-    while (!file.eof()) {
-        getline(file, students[n].id, '|');
-        getline(file, students[n].name, '|');
-        getline(file, students[n].password, '|');
-        file >> students[n].course_num;
-
-        // read course info
-        getline(file, courses);
-        string courses_arr[MAX_COURSES];
-        int total_credit_hour = 0;
-        for (int j = 0; j < students[n].course_num ; j++) { // loop every course of student
-            getline(file, students[n].course[j].code, '|');
-            getline(file, students[n].course[j].name, '|');
-            getline(file, students[n].course[j].gred);
-            students[n].course[j].credit_hour = int(students[n].course[j].code[8]);
-            total_credit_hour += students[n].course[j].credit_hour;
-        }
-        students[n].total_credit_hour = total_credit_hour;
-        n++;
-    }
-
-    file.close();
-}
-
-/**
- * @brief Write students info into targeted file
- * @param file_name Output file to be write
- * @param students Student array to write
- * @param student_num Number of students
-*/
-void writeFile(string file_name, Student students[], int student_num) {
-
-    fstream file(file_name, ios::out);
-
-    for (int i = 0; i < student_num; i++) {
-        // write student info
-        file << students[i].id << '|';
-        file << students[i].name << '|';
-        file << students[i].password << '|';
-        file << students[i].course_num;
-        //write course info
-        for (int j = 0; j < students[i].course_num; j++) {
-            file << endl;
-            file << students[i].course[j].code << '|';
-            file << students[i].course[j].name << '|';
-            file << students[i].course[j].gred;
-        }
-        // end of the student & course info
-
-        // if not last student, write new line char
-        if (i != student_num -1){
-            file << endl;
-        }
-    }
-    file.close();
 }
